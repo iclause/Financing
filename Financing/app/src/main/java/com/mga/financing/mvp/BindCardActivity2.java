3e50db47871ef29d60a51ee0fd01c1dc5eb61f8e
iclause <809016408@qq.com>
2018-06-01 19:25:48 +0800

    1

--- Financing/app/src/main/java/com/mga/financing/mvp/BindCardActivity2.java
+++ Financing/app/src/main/java/com/mga/financing/mvp/BindCardActivity2.java
@@ -1,19 +1,10 @@
 package com.mga.financing.mvp;
 
-import android.app.Dialog;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AlertDialog.Builder;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.mga.financing.R;
 import com.mga.financing.base.presenter.BasePresenter;
 import com.mga.financing.base.view.BaseActivity;
-import com.mga.financing.utils.UserInfoManager;
 
 /**
  * Created by mga on 2018/5/30 15:09.
@@ -23,6 +14,13 @@ public class BindCardActivity2 extends BaseActivity {
     @Override
     protected int getLayoutId() {
         return R.layout.activity_bind_card2;
+    }
+
+    @Override
+    protected void setupAppBar() {
+        super.setupAppBar();
+        mAppbarLeftBackIcon.setVisibility(View.VISIBLE);
+        mAppBarTitle.setText(getResources().getString(R.string.bind_card));
     }
 
     @Override
